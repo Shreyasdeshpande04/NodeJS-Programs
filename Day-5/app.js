46df408c0f9be935c8fdb129aab8b25ef34a1dce
Shreyas Deshpande <deshpandeshreyas434@gmail.com>
2025-09-07 20:16:33 +0530

    Deleted Day-5 folder

--- Day-5/app.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events')
-const event=new EventEmitter()
-
-event.on("welcome",(username)=>{
-    console.log(`welcome ${username}`);
-
-})
-
-event.emit("welcome","Shreyas")
